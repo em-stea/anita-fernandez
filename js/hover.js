09a88b9bbd5661742d197f63a36900964f035179
em-stea <emiliastea@hotmail.com>
2022-02-09 12:57:02 -0300

    upd

---
+++ js/hover.js
@@ -0,0 +1,71 @@
+$(".boton-1").hover(function () {
+  $(".tarjeta-1").css({
+    height: "654px",
+  });
+  $(".tarjeta-2").css({
+    height: "403px",
+  });
+  $(".tarjeta-3").css({
+    height: "403px",
+  });
+  $(".tarjeta-4").css({
+    height: "403px",
+  });
+  $(".contenedor-tarjetas-padre").css({
+    height: "175vh",
+  });
+});
+
+$(".boton-2").hover(function () {
+  $(".tarjeta-2").css({
+    height: "654px",
+  });
+  $(".tarjeta-1").css({
+    height: "403px",
+  });
+  $(".tarjeta-3").css({
+    height: "403px",
+  });
+  $(".tarjeta-4").css({
+    height: "403px",
+  });
+  $(".contenedor-tarjetas-padre").css({
+    height: "175vh",
+  });
+});
+
+$(".boton-3").hover(function () {
+  $(".tarjeta-3").css({
+    height: "654px",
+  });
+  $(".tarjeta-2").css({
+    height: "403px",
+  });
+  $(".tarjeta-1").css({
+    height: "403px",
+  });
+  $(".tarjeta-4").css({
+    height: "403px",
+  });
+  $(".contenedor-tarjetas-padre").css({
+    height: "175vh",
+  });
+});
+
+$(".boton-4").hover(function () {
+  $(".tarjeta-4").css({
+    height: "654px",
+  });
+  $(".tarjeta-3").css({
+    height: "403px",
+  });
+  $(".tarjeta-1").css({
+    height: "403px",
+  });
+  $(".tarjeta-2").css({
+    height: "403px",
+  });
+  $(".contenedor-tarjetas-padre").css({
+    height: "175vh",
+  });
+});
